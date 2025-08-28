b7955efd1bbb204c6df37d4c758ba74d4199e208
yuborong <rongyubogm@163.com>
2025-08-28 17:26:03 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -114,10 +114,38 @@ const API_SITES = {
         api: 'https://cj.lziapi.com/api.php/provide/vod/',
         name: '量子资源站'
     },
-    testSource: {
-        api: 'https://www.example.com/api.php/provide/vod',
-        name: '空内容测试源',
-        adult: true
+    ckzy: {
+    api: 'https://www.ckzy1.com/api.php/provide/vod',
+    name: 'CK资源'
+    },
+    jkun: {
+    api: 'https://jkunzyapi.com/api.php/provide/vod',
+    name: 'jkun资源'
+    },
+    bwzy: {
+    api: 'https://api.bwzym3u8.com/api.php/provide/vod',
+    name: '百万资源'
+    },
+    souav: {
+    api: 'https://api.souavzy.vip/api.php/provide/vod',
+    name: 'souav资源'
+    },
+    r155: {
+    api: 'https://155api.com/api.php/provide/vod',
+    name: '155资源'
+    },
+    lsb: {
+    api: 'https://apilsbzy1.com/api.php/provide/vod',
+    name: 'lsb资源'
+    },
+    huangcang: {
+    api: 'https://hsckzy.vip/api.php/provide/vod',
+    name: '黄色仓库',
+    detail: 'https://hsckzy.vip’
+    },
+    yutu: {
+    api: 'https://yutuzy10.com/api.php/provide/vod',
+    name: '玉兔资源'
     }
     //ARCHIVE https://telegra.ph/APIs-08-12
 };
